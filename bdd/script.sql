6363d1d0935df35690fbdf84a7102468d1e61e33
William <carindowilliam@gmail.com>
2023-01-10 09:25:08 +0100

    ajout des fichiers html,css

---
+++ bdd/script.sql
@@ -0,0 +1,29 @@
+drop database if exists web_projet;
+
+CREATE database web_projet;
+
+CREATE TABLE Station(
+   id INT,
+   nom VARCHAR(50),
+   etat VARCHAR(50),
+   velo VARCHAR(50),
+   place VARCHAR(50),
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE Utilisateur(
+   id INT,
+   nom VARCHAR(50),
+   mail VARCHAR(50),
+   num VARCHAR(50),
+   message VARCHAR(50),
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE Avis(
+   id INT,
+   pseudo VARCHAR(50),
+   message VARCHAR(50),
+   _date DATETIME,
+   PRIMARY KEY(id)
+);
