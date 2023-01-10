e38d41da3357d4147d4cd6ea9c696f98c3dda24f
Dylanreichart5 <114474867+Dylanreichart5@users.noreply.github.com>
2023-01-10 14:24:09 +0100

    change velo

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var contract = "Lyon";
 var api_key = "8f905b0bdd8ded93ab15d7ea8581dc703c2a1dea";
 var url = "https://api.jcdecaux.com/vls/v1/stations?contract=" + contract + "&apiKey=" + api_key;
+var myIcon = L.icon({
+    iconUrl: 'img/velo.png',
+    iconSize: [32, 32],
+});
  
  // Initialize the map and set its view to the coordinates of Lyon
-  var map = L.map('map').setView([45.764043, 4.835659], 13);
+  var map = L.map('map').setView([45.764043, 4.835659], 15);
 
   // Add a tile layer to the map
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -17,7 +21,7 @@ var url = "https://api.jcdecaux.com/vls/v1/stations?contract=" + contract + "&ap
     })
     .then(function(data) {
       data.forEach(function(station) {
-        var marker = L.marker([station.position.lat, station.position.lng]).addTo(map);
+        var marker = L.marker([station.position.lat, station.position.lng], {icon: myIcon}).addTo(map);
         marker.on('click', function() {
             document.getElementsByClassName('info')[0].innerHTML = `
             <div class="titre">INFO STATION</div>
@@ -30,5 +34,4 @@ var url = "https://api.jcdecaux.com/vls/v1/stations?contract=" + contract + "&ap
       });
     });
 
-
-
+    
