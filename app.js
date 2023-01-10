48b9c66134de15931e087a0646ff6b43dacec32c
Dylanreichart5 <114474867+Dylanreichart5@users.noreply.github.com>
2023-01-10 09:48:12 +0100

    Coordonnée de lyon sur le app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([45.764043, 4.835659], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
